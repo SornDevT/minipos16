08cd306cb807acb768a5f58358120420d0470688
SornDev <sorndev.teach@gmail.com>
2024-05-16 21:12:05 +0700

    install template to Laravel & install vue-router VDO 02

---
+++ resources/js/Router/index.js
@@ -0,0 +1,40 @@
+
+import { createWebHistory, createRouter } from "vue-router";
+
+import Store from "../Pages/Store.vue";
+import Pos from "../Pages/Pos.vue";
+import Transection from "../Pages/Transection.vue";
+import Report from "../Pages/Report.vue";
+
+export const routes = [
+    {
+        name: 'store',
+        path:'/',
+        component: Store
+    },
+    {
+        name: 'pos',
+        path:'/pos',
+        component: Pos
+    },
+    {
+        name: 'transection',
+        path:'/transection',
+        component: Transection
+    },
+    {
+        name: 'report',
+        path:'/report',
+        component: Report
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+    scrollBehavior () {
+        window.scrollTo(0,0)
+    }
+});
+
+export default router;
